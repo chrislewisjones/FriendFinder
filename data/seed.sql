21f6b25421a428081839d834d326e3b77afe09ec
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-09 16:40:58 -0500

    updated theme

--- data/seed.sql
+++ data/seed.sql
@@ -2,9 +2,9 @@ USE friendfinder_DB;
 
 insert into profiles(name, photo, scores)
 VALUES 
-("Arsenal", "https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/180px-Arsenal_FC.svg.png", "5,4,3,1,5,3,5,2"),
-("Liverpool", "https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Liverpool_FC.svg/220px-Liverpool_FC.svg.png", "4,5,4,5,1,3,4,2"),
-("Tottenham Hotspur", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Tottenham_Hotspur.svg/100px-Tottenham_Hotspur.svg.png", "5,1,2,1,2,1,1,2"),
-("Chelsea", "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/200px-Chelsea_FC.svg.png", "4,1,3,1,5,5,1,5"),
-("Manchester City", "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/200px-Manchester_City_FC_badge.svg.png", "5,1,5,5,2,4,5,5"),
-("Manchester United", "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/220px-Manchester_United_FC_crest.svg.png", "5,5,3,5,4,5,1,2");
+("Jemaine", "https://www.hbo.com/content/dam/hbodata/series/flight-of-the-conchords/character/jemaine-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg", "5,4,3,1,5,3,5,2"),
+("Bret", "https://www.hbo.com/content/dam/hbodata/series/flight-of-the-conchords/character/bret-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg", "4,5,4,5,1,3,4,2"),
+("Murray", "https://www.hbo.com/content/dam/hbodata/series/flight-of-the-conchords/character/murray-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg", "5,1,2,1,2,1,1,2"),
+("Mel", "https://www.hbo.com/content/dam/hbodata/series/flight-of-the-conchords/character/mel-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg", "4,1,3,1,5,5,1,5"),
+("Dave", "https://www.hbo.com/content/dam/hbodata/series/flight-of-the-conchords/character/dave-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg", "5,1,5,5,2,4,5,5");
+
