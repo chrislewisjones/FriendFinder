be839d033c2670e36068f0a46142d7649e5aa238
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-09 00:04:26 -0500

    updated theme

--- data/seed.sql
+++ data/seed.sql
@@ -2,6 +2,9 @@ USE friendfinder_DB;
 
 insert into profiles(name, photo, scores)
 VALUES 
-("Chris", "picUrl", "1,2,3,4,5,6,7,8,9,10"),
-("Chris2", "picUrl2", "2,3,4,5,6,7,8,9,10,1"),
-("Chris3", "picUrl3", "3,4,5,6,7,8,9,10,1,2");
+("Arsenal", "https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/180px-Arsenal_FC.svg.png", "5,4,3,1,5,3,5,2"),
+("Liverpool", "https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Liverpool_FC.svg/220px-Liverpool_FC.svg.png", "4,5,4,5,1,3,4,2"),
+("Tottenham Hotspur", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Tottenham_Hotspur.svg/100px-Tottenham_Hotspur.svg.png", "5,1,2,1,2,1,1,2"),
+("Chelsea", "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/200px-Chelsea_FC.svg.png", "4,1,3,1,5,5,1,5"),
+("Manchester City", "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/200px-Manchester_City_FC_badge.svg.png", "5,1,5,5,2,4,5,5"),
+("Manchester United", "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/220px-Manchester_United_FC_crest.svg.png", "5,5,3,5,4,5,1,2");
