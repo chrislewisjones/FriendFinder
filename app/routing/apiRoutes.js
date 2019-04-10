acff9120c62b972674ba05bb51884ab14c092cfc
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-09 22:40:10 -0500

    updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,25 @@
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+// var mysql = require("mysql");
+
+// var connection;
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "RootRoot",
+//     database: "friendfinder_DB"
+//   });
+// }
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     //once successfully connected, you may want to query your database for the info you'll need later!
+//   }
+// });
+
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -11,7 +30,6 @@ var connection = mysql.createConnection({
   database: "friendfinder_DB"
 });
 
-// Initiate MySQL Connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -20,35 +38,11 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/profile", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     connection.query("SELECT * FROM profiles", function(err, result) {
       res.json(result);
     });
   });
 };
 
-// API POST Requests
-// Below code handles when a user submits a form and thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate JavaScript array
-// (ex. User fills out a reservation request... this data is then sent to the server...
-// Then the server saves the data to the tableData array)
-// ---------------------------------------------------------------------------
-
-// ---------------------------------------------------------------------------
-// I added this below code so you could clear out the table while working with the functionality.
-// Don"t worry about it!
-
-//   app.post("/api/clear", function(req, res) {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
-
-//     res.json({ ok: true });
-//   });
+//add post route
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-
-
 var express = require("express");
+var mysql = require("mysql");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./app/public")); // allows use of style.css
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
